1e88de12c42004828259c103e1bcbbc8be6ed0db
SwartzJonah <swartzjonah@gmail.com>
2023-03-20 13:46:45 -0400

    power function

--- recursion-practice/main.js
+++ recursion-practice/main.js
@@ -5,5 +5,12 @@ function sumRange(n){
     return n + sumRange(n-1)
 }
 
-let answer = sumRange(3);
+function power(base, exp){
+    if (exp <= 1){
+        return base;
+    }
+    return base * power(base, exp - 1);
+}
+
+let answer = power(3,5);
 console.log(answer);
