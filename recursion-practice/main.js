54efe874faefb50255ab5c0ff00a9962e6061842
SwartzJonah <swartzjonah@gmail.com>
2023-03-20 14:26:03 -0400

    replicate function

--- recursion-practice/main.js
+++ recursion-practice/main.js
@@ -111,6 +111,16 @@ function sumSquares(array){
     return total + sumSquares(array);
 }
 
+function replicate(times, num){
+    if (times <= 0){
+        return [];
+    }
+    return [num].concat(replicate(times - 1, num));
+}
+
+console.log(replicate(3, 5)) // [5, 5, 5]
+console.log(replicate(1, 69)) // [69]
+console.log(replicate(-2, 6)) // []
 
 // let answer = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]);
 // console.log(answer);
